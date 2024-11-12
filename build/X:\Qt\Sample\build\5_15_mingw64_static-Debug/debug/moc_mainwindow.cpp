2e15460eba0321a633fd91ceb0300023c3ebf472
Aldar <ald.ald.94@bk.ru>
2024-11-12 14:46:21 +0300

    Add files via upload

--- build/X:\Qt\Sample\build\5_15_mingw64_static-Debug/debug/moc_mainwindow.cpp
+++ build/X:\Qt\Sample\build\5_15_mingw64_static-Debug/debug/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[10];
-    char stringdata0[142];
+    QByteArrayData data[12];
+    char stringdata0[185];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -41,13 +41,16 @@ QT_MOC_LITERAL(5, 51, 16), // "on_Equil_pressed"
 QT_MOC_LITERAL(6, 68, 17), // "on_Equil_released"
 QT_MOC_LITERAL(7, 86, 16), // "on_Clear_clicked"
 QT_MOC_LITERAL(8, 103, 18), // "onHoldTimerTimeout"
-QT_MOC_LITERAL(9, 122, 19) // "onInputTimerTimeout"
+QT_MOC_LITERAL(9, 122, 19), // "onInputTimerTimeout"
+QT_MOC_LITERAL(10, 142, 16), // "on_Equil_clicked"
+QT_MOC_LITERAL(11, 159, 25) // "on_pushButton_Doc_clicked"
 
     },
     "MainWindow\0digits_numbers\0\0operations\0"
     "setOperation\0on_Equil_pressed\0"
     "on_Equil_released\0on_Clear_clicked\0"
-    "onHoldTimerTimeout\0onInputTimerTimeout"
+    "onHoldTimerTimeout\0onInputTimerTimeout\0"
+    "on_Equil_clicked\0on_pushButton_Doc_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -57,7 +60,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       8,   14, // methods
+      10,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -65,16 +68,20 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   54,    2, 0x08 /* Private */,
-       3,    0,   55,    2, 0x08 /* Private */,
-       4,    0,   56,    2, 0x08 /* Private */,
-       5,    0,   57,    2, 0x08 /* Private */,
-       6,    0,   58,    2, 0x08 /* Private */,
-       7,    0,   59,    2, 0x08 /* Private */,
-       8,    0,   60,    2, 0x08 /* Private */,
-       9,    0,   61,    2, 0x08 /* Private */,
+       1,    0,   64,    2, 0x08 /* Private */,
+       3,    0,   65,    2, 0x08 /* Private */,
+       4,    0,   66,    2, 0x08 /* Private */,
+       5,    0,   67,    2, 0x08 /* Private */,
+       6,    0,   68,    2, 0x08 /* Private */,
+       7,    0,   69,    2, 0x08 /* Private */,
+       8,    0,   70,    2, 0x08 /* Private */,
+       9,    0,   71,    2, 0x08 /* Private */,
+      10,    0,   72,    2, 0x08 /* Private */,
+      11,    0,   73,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -101,6 +108,8 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 5: _t->on_Clear_clicked(); break;
         case 6: _t->onHoldTimerTimeout(); break;
         case 7: _t->onInputTimerTimeout(); break;
+        case 8: _t->on_Equil_clicked(); break;
+        case 9: _t->on_pushButton_Doc_clicked(); break;
         default: ;
         }
     }
@@ -136,13 +145,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 8)
+        if (_id < 10)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 8;
+        _id -= 10;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 8)
+        if (_id < 10)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 8;
+        _id -= 10;
     }
     return _id;
 }
